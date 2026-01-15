42fee43dd0b3b688cea0056794736d3580a98d19
Silas Gardin <silasdetoledo@gmail.com>
2026-01-15 15:07:52 -0300

    Create dia_de_sorte.py

---
+++ motores/dia_de_sorte.py
@@ -0,0 +1,8 @@
+from .base import MotorBase
+
+class MotorDiaDeSorte(MotorBase):
+    """
+    Específico para Dia de Sorte (31 números).
+    Futuramente podemos adicionar lógica para o 'Mês de Sorte'.
+    """
+    pass
