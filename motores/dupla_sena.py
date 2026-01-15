add6a1245769964ad47255012f17e535e6c6c34a
Silas Gardin <silasdetoledo@gmail.com>
2026-01-15 15:08:13 -0300

    Create dupla_sena.py

---
+++ motores/dupla_sena.py
@@ -0,0 +1,8 @@
+from .base import MotorBase
+
+class MotorDuplaSena(MotorBase):
+    """
+    Dupla Sena tem dois sorteios por concurso.
+    Por padrão, analisamos o primeiro sorteio (que paga mais).
+    """
+    pass
